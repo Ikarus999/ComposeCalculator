07bd197c613d384f1b1c9fa91d04132e9f3b5a83
Abdul Samad <32610065+Ikarus999@users.noreply.github.com>
2023-07-03 18:21:16 +0500

    initial commit.

---
+++ app/src/main/java/com/icarus/composecalculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.icarus.composecalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.icarus.composecalculator.ui.layouts.Calculator
+import com.icarus.composecalculator.ui.theme.ComposeCalculatorTheme
+import com.icarus.composecalculator.ui.theme.MediumGray
+import com.icarus.composecalculator.ui.viewmodel.CalculatorViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction =viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
